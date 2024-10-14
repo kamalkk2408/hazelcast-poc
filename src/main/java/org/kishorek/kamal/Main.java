750f08d4c4e95cd81af48647ef629a9e394b1d70
kamal <kamalkishore2408@gmail.com>
2024-10-15 01:11:21 +0530

    Hazel cast dependency added

--- src/main/java/org/kishorek/kamal/Main.java
+++ src/main/java/org/kishorek/kamal/Main.java
@@ -1,17 +1,28 @@
 package org.kishorek.kamal;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        processHazelcast();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    private static void processHazelcast(){
+        HazelcastInstance hz = Hazelcast.newHazelcastInstance();
+        IMap<Integer, String> myMap = hz.getMap("myMap");
+
+        for(int i =0; i< 5;i++){
+            myMap.set(i, "value = "+i);
         }
+
+        myMap.forEach((key, value) -> {
+            System.out.println("Key: "+key + ", Value: "+value);
+        });
     }
 }
